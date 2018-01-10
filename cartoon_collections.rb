73a507d8e3002a55eff26c93b4f215c859acf37a
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-10 00:41:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ def summon_captain_planet(elements)
  elements.map {|element| puts "#{element.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
